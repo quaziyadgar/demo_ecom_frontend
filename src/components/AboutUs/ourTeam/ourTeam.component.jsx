c46e76c5d3714b993c34361398b770ab2df02ffe
Quazi Yadgar Husain <quazi.yadgar@wokegenics.com>
2024-08-10 10:34:11 +0530

    some fixes

--- src/components/AboutUs/ourTeam/ourTeam.component.jsx
+++ src/components/AboutUs/ourTeam/ourTeam.component.jsx
@@ -8,6 +8,45 @@ import person4 from "assets/ourTeam/person_4.jpg";
 
 import "./ourTeam.component.css";
 
+const ourTeam = [
+  {
+    dp: person1,
+    firstName: "Lawson",
+    lastName: "Harrison",
+    designation: "CEO, Founder, Atty",
+    description:
+      "Separated they live in. Separated they live in Bookmarksgrove right at the coast of the Semantics, Link large language ocean",
+    link: "#",
+  },
+  {
+    dp: person2,
+    firstName: "Jeremy",
+    lastName: "Walker",
+    designation: "CEO, Founder, Atty",
+    description:
+      "Separated they live in. Separated they live in Bookmarksgrove right at the coast of the Semantics, Link large language ocean",
+    link: "#",
+  },
+  {
+    dp: person3,
+    firstName: "Patrik",
+    lastName: "White",
+    designation: "CEO, Founder, Atty",
+    description:
+      "Separated they live in. Separated they live in Bookmarksgrove right at the coast of the Semantics, Link large language ocean",
+    link: "#",
+  },
+  {
+    dp: person4,
+    firstName: "Kathryn",
+    lastName: "Ryan",
+    designation: "CEO, Founder, Atty",
+    description:
+      "Separated they live in. Separated they live in Bookmarksgrove right at the coast of the Semantics, Link large language ocean",
+    link: "#",
+  },
+];
+
 export const OurTeam = () => {
   return (
     <div className="untree_co-section">
@@ -19,85 +58,26 @@ export const OurTeam = () => {
         </div>
         <div className="row">
           {/* Start Column 1 */}
-          <div className="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-            <img alt="" src={person1} className="img-fluid mb-5" />
-            <h3>
-              <Link to="#">
-                <span>Lawson</span> Arnold
-              </Link>
-            </h3>
-            <span className="d-block position mb-4">CEO, Founder, Atty.</span>
-            <p>
-              Separated they live in. Separated they live in Bookmarksgrove
-              right at the coast of the Semantics, Link large language ocean.
-            </p>
-            <p className="mb-0">
-              <Link to="#" className="more dark">
-                Learn More <span className="icon-arrow_forward" />
-              </Link>
-            </p>
-          </div>
+          {ourTeam.map((key) => (
+            <div className="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
+              <img alt="" src={key.dp} className="img-fluid mb-5" />
+              <h3>
+                <Link to="#">
+                  <span>{key.firstName}</span> {key.lastName}
+                </Link>
+              </h3>
+              <span className="d-block position mb-4">{key.designation}.</span>
+              <p>
+               {key.description}
+              </p>
+              <p className="mb-0">
+                <Link to={key.link} className="more dark">
+                  Learn More <span className="icon-arrow_forward" />
+                </Link>
+              </p>
+            </div>
+          ))}
           {/* End Column 1 */}
-          {/* Start Column 2 */}
-          <div className="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-            <img alt="" src={person2} className="img-fluid mb-5" />
-            <h3>
-              <Link to="#">
-                <span>Jeremy</span> Walker
-              </Link>
-            </h3>
-            <span className="d-block position mb-4">CEO, Founder, Atty.</span>
-            <p>
-              Separated they live in. Separated they live in Bookmarksgrove
-              right at the coast of the Semantics, Link large language ocean.
-            </p>
-            <p className="mb-0">
-              <Link to="#" className="more dark">
-                Learn More <span className="icon-arrow_forward" />
-              </Link>
-            </p>
-          </div>
-          {/* End Column 2 */}
-          {/* Start Column 3 */}
-          <div className="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-            <img alt="" src={person3} className="img-fluid mb-5" />
-            <h3>
-              <Link to="#">
-                <span>Patrik</span> White
-              </Link>
-            </h3>
-            <span className="d-block position mb-4">CEO, Founder, Atty.</span>
-            <p>
-              Separated they live in. Separated they live in Bookmarksgrove
-              right at the coast of the Semantics, Link large language ocean.
-            </p>
-            <p className="mb-0">
-              <Link to="#" className="more dark">
-                Learn More <span className="icon-arrow_forward" />
-              </Link>
-            </p>
-          </div>
-          {/* End Column 3 */}
-          {/* Start Column 4 */}
-          <div className="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-            <img alt="" src={person4} className="img-fluid mb-5" />
-            <h3>
-              <Link to="#">
-                <span>Kathryn</span> Ryan
-              </Link>
-            </h3>
-            <span className="d-block position mb-4">CEO, Founder, Atty.</span>
-            <p>
-              Separated they live in. Separated they live in Bookmarksgrove
-              right at the coast of the Semantics, Link large language ocean.
-            </p>
-            <p className="mb-0">
-              <Link to="#" className="more dark">
-                Learn More <span className="icon-arrow_forward" />
-              </Link>
-            </p>
-          </div>
-          {/* End Column 4 */}
         </div>
       </div>
     </div>
--- src/components/Navbar/navbar.component.jsx
+++ src/components/Navbar/navbar.component.jsx
@@ -20,15 +20,32 @@ export const MyNavbar = () => {
             Furni<span>.</span>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="position-relative">
-            <Nav className="me-auto position-absolute end-0">
-              <Link to="/" className="mx-3">Home</Link>
-              <Link to="/shop" className="mx-3">Shop</Link>
-              <Link to="/about" className="mx-3">About us</Link>
-              <Link to="/services" className="mx-3">Services</Link>
-              <Link to="/blog" className="mx-3">Blog</Link>
-              <Link to="/contact" className="mx-3">Contact Us</Link>
-              <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav className="">
+              <ul className="custom-navbar-nav navbar-nav mb-2 mb-md-0">
+                <Link to="/" className="mx-3 my-2">
+                  Home
+                </Link>
+                <Link to="/shop" className="mx-3 my-2">
+                  Shop
+                </Link>
+                <Link to="/about" className="mx-3 my-2">
+                  About us
+                </Link>
+                <Link to="/services" className="mx-3 my-2">
+                  Services
+                </Link>
+                <Link to="/blog" className="mx-3 my-2">
+                  Blog
+                </Link>
+                <Link to="/contact" className="mx-3 my-2">
+                  Contact Us
+                </Link>
+              </ul>
+              <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5 my-2">
                 <Link to="#">
                   <User className="nav-icon" />
                 </Link>
